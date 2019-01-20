8a7661b78310db450375d91fe3d48ebed6527821
anandmuraleedharan <anand.muraleedharan@gmail.com>
2019-01-20 14:35:30 -0600

    Big Data Course - Python and Spark - Frank Kane

---
+++ total-spent-by-customer-sorted.py
@@ -0,0 +1,17 @@
+from pyspark import SparkConf, SparkContext
+
+conf = SparkConf().setMaster("local").setAppName("SpendByCustomer")
+sc = SparkContext(conf = conf)
+
+def extractCustomerPricePairs(line):
+    fields = line.split(',')
+    return (int(fields[0]), float(fields[2]))
+
+input = sc.textFile("file:///sparkcourse/customer-orders.csv")
+mappedInput = input.map(extractCustomerPricePairs)
+totalByCustomer = mappedInput.reduceByKey(lambda x, y: x + y)
+sortedTotalByCustomer = totalByCustomer.map(lambda x: (x[1],x[0])).sortByKey()
+
+results = sortedTotalByCustomer.collect();
+for result in results:
+    print(result)
